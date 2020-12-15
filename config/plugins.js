77060f4366588ab3c04a2828172f4247e48079be
Robert Gühne <robert@polyxostudios.de>
2020-12-15 17:43:11 +0100

    feat(Devices): new react media

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,21 @@
 module.exports = ({ env }) => ({
   upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET"),
+      },
+    },
+  },
+  /*upload: {
     provider: "cloudinary",
     providerOptions: {
       cloud_name: env("CLOUDINARY_NAME"),
       api_key: env("CLOUDINARY_KEY"),
       api_secret: env("CLOUDINARY_SECRET"),
     },
-  },
+  },*/
 });
